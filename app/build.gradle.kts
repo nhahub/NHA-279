1198435fba60837127896ce5120926c006e21faf
moazelgebaly25 <moaz.elgebaly.25@gmail.com>
2025-12-19 19:44:14 +0200

    fix: update Retrofit dependencies to stable 2.x versions
    - Update retrofit from 3.0.0 to 2.11.0
    
    - Update converter-moshi from 3.0.0 to 2.11.0
    
    - Fixes build failure due to non-existent 3.0.0 artifacts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -161,8 +161,8 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.7.0")
 
     // Networking
-    implementation("com.squareup.retrofit2:retrofit:3.0.0")
-    implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
     implementation("com.squareup.okhttp3:logging-interceptor:5.3.0")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.2")
